bc9fc05e6805e10883fc41757416278169af144e
Foysal Ahmed <foysalxahmed@gmail.com>
2023-08-31 05:47:43 +0600

    Add closure example

--- src/_javascript_practice/intermediate/closure_javascript.js
+++ src/_javascript_practice/intermediate/closure_javascript.js
@@ -1,7 +1,7 @@
 let clientName = 'John'
 
 function printMyName() {
-    console.log(clientName)
+  console.log(clientName)
 }
 
 //prints John, because whole javascript file is a big closure and every function has the access
@@ -13,25 +13,25 @@ clientName = 'Joey'
 printMyName() //prints Joey, because constantly goes with the most recent value of any variable thats being called or used inside function
 
 function outerFunction(outerVariable) {
-    return function innerFunction(innerVariable) {
-        console.log('Outer variable:', outerVariable)
-        console.log('Inner variable:', innerVariable)
-    }
+  return function innerFunction(innerVariable) {
+    console.log('Outer variable:', outerVariable)
+    console.log('Inner variable:', innerVariable)
+  }
 }
 const newFunction = outerFunction('Outside')
 newFunction('Inside')
 
 function testFunction() {
-    var a = 45
-    console.log(a)
+  var a = 45
+  console.log(a)
 }
 
 var a, b
 if (a === b) {
-    var a = 4
-    let b = 5
-    const c = 6
-    console.log('true', a)
+  var a = 4
+  let b = 5
+  const c = 6
+  console.log('true', a)
 }
 console.log({ a, b, c }) //* c is not defined
 
@@ -42,13 +42,13 @@ console.log({ a, b, c }) //* c is not defined
 //* Javascript creates closure of any variable that we use  in a function
 
 function counter() {
-    let count = 0
-    let testUnusedVariable = 45 //* This will not be in the closure because not used in the following function
-    return function () {
-        debugger
-        count++
-        console.log('Hey!!', count)
-    }
+  let count = 0
+  let testUnusedVariable = 45 //* This will not be in the closure because not used in the following function
+  return function () {
+    debugger
+    count++
+    console.log('Hey!!', count)
+  }
 }
 
 let result = counter()
@@ -56,3 +56,14 @@ result()
 result()
 result()
 
+function init() {
+  var name = 'Mozilla' // name is a local variable created by init
+  function displayName() {
+    // displayName() is the inner function, a closure
+    console.log(name) // use variable declared in the parent function
+  }
+  displayName()
+}
+
+var name = 'Chrome'
+init()
